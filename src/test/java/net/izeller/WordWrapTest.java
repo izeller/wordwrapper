828473109566b7d2dc3d0e469a208c31f279026e
izeller <irenezeller@gmail.com>
2016-04-10 18:42:21 +0200

    Add enough space line- Red

---
+++ src/test/java/net/izeller/WordWrapTest.java
@@ -0,0 +1,20 @@
+package net.izeller;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+/*
+ * @author irene, @date 10/04/16 18:07
+ */
+public class WordWrapTest {
+	
+    @Test 
+    public void enough_length_line() {
+       assertThat(wrap("hola", 5), is("hola"));
+    }
+    
+    public String wrap(String line, int lengthLine){
+    	return null;
+    }
+}
