927af5479b856717d378f537c71545f385bd9544
izeller <irenezeller@gmail.com>
2016-04-10 20:19:59 +0200

    Split one word and spaces- RED

--- src/test/java/net/izeller/WordWrapTest.java
+++ src/test/java/net/izeller/WordWrapTest.java
@@ -8,54 +8,65 @@ import org.junit.Test;
  * @author irene, @date 10/04/16 18:07
  */
 public class WordWrapTest {
+
+	@Test 
+	public void enough_length_line() {
+		assertThat(wrap("hola", 5), is("hola"));
+	}
+
+	@Test 
+	public void split_two_words() {
+		assertThat(wrap("hola TDD", 5), is("hola\nTDD"));
+	}
+
+	@Test 
+	public void split_three_words_in_two_lines() {
+		assertThat(wrap("hola TDD wordWrap", 8), is("hola TDD\nwordWrap"));
+	}
+	@Test 
+	public void split_three_words_in_three_lines() {
+		assertThat(wrap("hola TDD word", 4), is("hola\nTDD\nword"));
+	}
+
+	@Test
+	public void split_one_word_in_two_lines(){
+		assertThat(wrap("holaTDD", 4), is("hola\nTDD"));
+	}
+
+	@Test
+	public void split_one_word_in_many_lines(){
+		assertThat(wrap("holaTDD", 2), is("ho\nla\nTD\nD"));
+	}
 	
-    @Test 
-    public void enough_length_line() {
-       assertThat(wrap("hola", 5), is("hola"));
-    }
-    
-    @Test 
-    public void split_two_words() {
-       assertThat(wrap("hola TDD", 5), is("hola\nTDD"));
-    }
-    
-    @Test 
-    public void split_three_words_in_two_lines() {
-       assertThat(wrap("hola TDD wordWrap", 8), is("hola TDD\nwordWrap"));
-    }
-    @Test 
-    public void split_three_words_in_three_lines() {
-       assertThat(wrap("hola TDD word", 4), is("hola\nTDD\nword"));
-    }
-    
-    @Test
-    public void split_one_word_in_two_lines(){
-    	 assertThat(wrap("holaTDD", 4), is("hola\nTDD"));
-    }
-    
-    @Test
-    public void split_one_word_in_many_lines(){
-    	 assertThat(wrap("holaTDD", 2), is("ho\nla\nTD\nD"));
-    }
-    public String wrap(String line, int lengthLine){
-    	
-    	if(line.length()>lengthLine){
-    		int indexOfSpace = indexOfFirstSpace(line, lengthLine);
-    		if(indexOfSpace<0){
-    			return wrappedLine(line, lengthLine)+
-    					"\n"+
-    					wrap(notWrappedLine(line, lengthLine-1), lengthLine);
-    		}
-    		return wrappedLine(line, indexOfSpace)+
-    				"\n"+
-    				wrap(notWrappedLine(line, indexOfSpace), lengthLine);
-    	}else{
-    		return line;	
-    	}
-    	
-    }
-    
-    private String notWrappedLine(String line, int indexOfSpace) {
+	@Test
+	public void split_word_and_spaces(){
+		assertThat(wrap("hola TDD", 3), is("hol\na\nTDD"));
+	}
+
+	public String wrap(String line, int lengthLine){
+		if(enoughSpace(line, lengthLine)){
+			return line;
+		}
+
+		int indexOfSpace = indexOfFirstSpace(line, lengthLine);
+		if(indexOfSpace<0 ){
+			return wrappedLine(line, lengthLine)+
+					"\n"+
+					wrap(notWrappedLine(line, lengthLine-1), lengthLine);
+		}
+		return wrappedLine(line, indexOfSpace)+
+				"\n"+
+				wrap(notWrappedLine(line, indexOfSpace), lengthLine);
+
+
+	}
+
+	private boolean enoughSpace(String line, int lengthLine) {
+
+		return line.length()<=lengthLine;
+	}
+
+	private String notWrappedLine(String line, int indexOfSpace) {
 		return line.substring(indexOfSpace+1);
 	}
 
